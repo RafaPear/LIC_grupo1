5c8646883d6d8226d7fe9911006d5a52e0b4fadb
RafaPear <rafapear@outlook.pt>
2025-02-26 10:02:49 +0000

    Resolved Some Penc Errors

--- Hardware/KeyScanner/Penc.vhd
+++ Hardware/KeyScanner/Penc.vhd
@@ -5,7 +5,7 @@ entity PENC is
   port (
     I: in std_logic_vector (3 downto 0);
     Y: out std_logic_vector (3 downto 0);
-    GS: out std_logic;
+    GS: out std_logic
     );
 end PENC;
 
@@ -13,7 +13,5 @@ architecture arc_penc of PENC is
     begin
       GS <= I(0) or I(1) or I(2) or I(3);
       Y(1) <= I(3) or I(2);
-      Y(0) <= I(3) or (not I(2) and I(3));
-end arc_penc
-    
-
+      Y(0) <= I(3) or (not I(2) and I(3))
+end arc_penc;
