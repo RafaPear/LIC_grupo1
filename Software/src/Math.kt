93f399aa51100f4a9b4111b0a688aa352b717ce3
RafaPear <rafapear@outlook.pt>
2025-03-19 11:23:56 +0000

    Enhances LCD communication by implementing serial and parallel nibble writing, and adds utility functions for bit manipulation

--- Software/src/Math.kt
+++ Software/src/Math.kt
@@ -1,3 +1,5 @@
+import kotlin.countOneBits
+
 fun pow(base: Int, exponent: Int): Int{
     var temp = 1
     for (i in 1..exponent){
@@ -8,4 +10,12 @@ fun pow(base: Int, exponent: Int): Int{
 
 fun Int.toBitPlace(): Int{
     return pow(2, this)
+}
+
+fun Int.isBit(index: Int) : Boolean{
+    return and(1.shl(index)).countOneBits() == 1
+}
+
+fun Boolean.toInt(): Int{
+    return if (this) 1 else 0
 }
