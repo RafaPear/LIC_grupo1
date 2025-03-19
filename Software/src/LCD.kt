e7b003b3281875a4efa9dbe33a727c6791f8f68d
RafaPear <rafapear@outlook.pt>
2025-03-19 10:56:04 +0000

    Implements serial communication for LCD, optimizes nibble writing, and enhances data handling

--- Software/src/LCD.kt
+++ Software/src/LCD.kt
@@ -3,6 +3,7 @@ object LCD {
     // Dimensão do display.
     private const val LINES = 2
     private const val COLS = 16
+    private const val NIBBLE = 3
 
     // Define se a interface é Série ou Paralela.
     private const val SERIAL_INTERFACE = false
@@ -11,35 +12,58 @@ object LCD {
     private fun writeNibbleParallel(rs: Boolean, data: Int) { /* Implementação */ }
 
     // Escreve um nibble de comando/dados no LCD em série.
-    private fun writeNibbleSerial(rs: Boolean, data: Int) { /* Implementação */ }
+    private fun writeNibbleSerial(rs: Boolean, data: Int) {
+        // Envia E On
+        HAL.setBits(1)
+        // Envia rs
+        HAL.setBits(0b0000_0001)
+        // Envia data
+        for (i in NIBBLE..0) {
+            val bit = HAL.isBit(1.shl(i))
+            HAL.writeBits(1, if (bit) 0b0000_0001 else 0b0000_0000)
+        }
+        // Envia E Off
+        HAL.clrBits(1)
+    }
 
     // Escreve um nibble de comando/dados no LCD.
     private fun writeNibble(rs: Boolean, data: Int) {
-        if (SERIAL_INTERFACE)
-            writeNibbleSerial(rs, data)
-        else
-            writeNibbleParallel(rs, data)
+        if (data.countOneBits() < 5){
+            if (SERIAL_INTERFACE)
+                writeNibbleSerial(rs, data)
+            else
+                writeNibbleParallel(rs, data)
+        }
     }
 
     // Escreve um byte de comando/dados no LCD.
     private fun writeByte(rs: Boolean, data: Int) {
-
+        writeNibble(rs, data shr 4)
+        writeNibble(rs, data and 0b0000_1111)
     }
 
     // Escreve um comando no LCD.
-    private fun writeCMD(data: Int) { /* Implementação */ }
+    private fun writeCMD(data: Int) {
+        writeByte(false, data)
+    }
 
     // Escreve um dado no LCD.
-    private fun writeDATA(data: Int) { /* Implementação */ }
+    private fun writeDATA(data: Int) {
+        writeByte(true, data)
+    }
 
     // Envia a sequência de iniciação para comunicação a 4 bits.
     fun init() { /* Implementação */ }
 
     // Escreve um caractere na posição corrente.
-    fun write(c: Char) { /* Implementação */ }
+    fun write(c: Char) {
+        writeDATA(c.code)
+    }
 
     // Escreve uma string na posição corrente.
-    fun write(text: String) { /* Implementação */ }
+    fun write(text: String) {
+
+    }
 
     // Envia comando para posicionar o cursor.
     // (line: 0..LINES-1, column: 0..COLS-1)
