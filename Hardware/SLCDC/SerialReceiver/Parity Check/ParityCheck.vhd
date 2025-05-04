552b27299bb058d000992ac20395c8232783071f
IanFrunze <ian.frunze06@gmail.com>
2025-05-04 21:06:18 +0100

    Adjusted Counter, ParityCheck, SerialControl, and SerialReceiver and SerialReceiver_tb components; add testbench for Counter; update signal handling and architecture to improve functionality.

--- Hardware/SLCDC/SerialReceiver/Parity Check/ParityCheck.vhd
+++ Hardware/SLCDC/SerialReceiver/Parity Check/ParityCheck.vhd
@@ -3,28 +3,26 @@ use ieee.std_logic_1164.all;
 
 entity ParityCheck is
     port(
-        clk: in std_logic;
-        init: in std_logic;
-        data_in: in std_logic;
-        error: out std_logic
+        clk     : in std_logic;
+        init    : in std_logic;
+        data_in : in std_logic;
+        error   : out std_logic
     );
 end ParityCheck;
 
 architecture Behavioral of ParityCheck is
     signal Q: std_logic := '0';
-    signal xor_out: std_logic;
-    
 begin
-    xor_out <= data_in xor Q;
-
-    process (clk, init)
+    process(clk)
     begin
-        if init = '1' then
-            Q <= '0';
-        elsif rising_edge(clk) then
-            Q <= xor_out;
+        if rising_edge(clk) then
+            if init = '1' then
+                Q <= '0';
+            else
+                Q <= Q xor data_in;
+            end if;
         end if;
     end process;
 
-    error <= not Q;
+    error <= Q;
 end Behavioral;
