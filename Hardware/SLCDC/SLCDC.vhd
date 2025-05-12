30a346b820585f87593fc23e6af6c9410e959ac7
RafaPear <rafapear@outlook.pt>
2025-05-12 23:28:07 +0100

    Add SLCDC entity and architecture implementation with SerialReceiver and LCDDispacher components
    Adds SLCDC entity with SerialReceiver and LCDDispatcher
    
    Implements the SLCDC entity and architecture, integrating SerialReceiver and LCDDispatcher components for signal handling and data dispatching. Removes outdated binary file.
    
    Enhances modularity and functionality of the hardware design.

---
+++ Hardware/SLCDC/SLCDC.vhd
@@ -0,0 +1,69 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity SLCDC is
+    port(
+        clk: in std_logic;
+        RESET: in std_logic;
+        LCDsel: in std_logic;
+        SCLK: in std_logic;
+        SDX: in std_logic;
+        E: out std_logic;
+        Dout: out std_logic_vector(4 downto 0)
+    );
+end SLCDC;
+
+architecture arch_SLCDC of SLCDC is
+    component SerialReciever is
+        port(
+            SDX: in std_logic;
+            SCLK: in std_logic;
+            clk_control: in std_logic;
+            SS: in std_logic;
+            accept: in std_logic;
+            RESET: in std_logic;
+            D: out std_logic_vector(4 downto 0);
+            DX_val: out std_logic
+        );
+    end component;
+
+    component LCDDispacher is
+        port(
+            Dval: in std_logic;
+            Din: in std_logic_vector(4 downto 0);
+            clk: in std_logic;
+            Wrl: out std_logic;
+            Dout: out std_logic_vector(4 downto 0);
+            done: out std_logic
+        );
+    end component;
+
+    signal DXval_s, done_s: std_logic;
+    signal D_s: std_logic_vector(4 downto 0);
+
+
+    begin
+    -- Instantiate the SerialReceiver
+    SerialReceiver_inst: SerialReciever
+        port map(
+            SDX => SDX,
+            SCLK => SCLK,
+            clk_control => clk,
+            SS => LCDsel,
+            accept => done_s,
+            RESET => RESET,
+            D => D_s,
+            DX_val => DXval_s
+        );
+    
+    -- Instantiate the LCDDispacher
+    LCDDispacher_inst: LCDDispacher
+        port map(
+            Dval => DXval_s,
+            Din => D_s,
+            clk => clk,
+            Wrl => E,
+            Dout => Dout,
+            done => done_s
+        );
+end arch_SLCDC;
